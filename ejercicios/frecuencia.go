a4cc9e24e71de6f6f9da70410dd256969099e2a4
CarlosMauricioAristizabal <cmabueno@gmail.com>
2023-04-13 22:04:45 -0300

    Ejercicios en clase

--- ejercicios/frecuencia.go
+++ ejercicios/frecuencia.go
@@ -2,8 +2,23 @@ package ejercicios
 
 import (
 	"guia6/dictionary"
+
+	"github.com/agrison/go-commons-lang/stringUtils"
 )
 
-func Frecuencia(s string) *dictionary.Dictionary[string, int]{
-	panic("Not implemented")
+func Frecuencia(s string) *dictionary.Dictionary[string, int] {
+
+	var frecuencia = dictionary.NewDictionary[string, int]()
+
+	for _, letra := range s {
+		if stringUtils.IsAlpha(string(letra)) {
+			if frecuencia.Contains(string(letra)) {
+				frecuencia.Put(string(letra), frecuencia.Get(string(letra))+1)
+
+			} else {
+				frecuencia.Put(string(letra), 1)
+			}
+		}
+	}
+	return &frecuencia
 }
